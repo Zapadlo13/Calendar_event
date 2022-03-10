62d4c3b222d52dcda9a1612555a86b6e23273b01
Zapadlo <zapadlo_13@mail.ru>
2022-03-10 15:43:50 +0300

    add model Event

--- cal/models.py
+++ cal/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    title = models.CharField(max_length=128, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание', blank=True)
+    start_time = models.DateTimeField(verbose_name='Начало события', blank=True)
+    end_time = models.DateTimeField(verbose_name='Конец события', blank=True)
+    create = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
